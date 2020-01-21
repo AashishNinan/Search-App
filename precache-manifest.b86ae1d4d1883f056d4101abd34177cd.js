4dd75e11a581b346f00242c5b3afee4f67782c41
AashishNinan <aashishninan@gmai.com>
2020-01-21 10:43:49 +0530

    Updates

---
+++ precache-manifest.b86ae1d4d1883f056d4101abd34177cd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f316dc6ba4dcc954561688aa7ce9858",
+    "url": "/Search-App/index.html"
+  },
+  {
+    "revision": "1f81e3adc2906072c6e9",
+    "url": "/Search-App/static/css/main.b0f8acc5.chunk.css"
+  },
+  {
+    "revision": "e09f087fa30fbeabb886",
+    "url": "/Search-App/static/js/2.7f35f9e2.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Search-App/static/js/2.7f35f9e2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1f81e3adc2906072c6e9",
+    "url": "/Search-App/static/js/main.00bb55cc.chunk.js"
+  },
+  {
+    "revision": "395ddc7f774ac909f19a",
+    "url": "/Search-App/static/js/runtime-main.f68c7329.js"
+  }
+]);
